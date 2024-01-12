499ccc4dcce6ce910deb3dbc4a22ef201335d55c
contactserus@gmail.com <[contact-serus@gmail.com]>
2024-01-12 09:10:10 +0100

    fix: new routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import Cart from "./pages/Cart";
+import Blog from "./pages/Blog";
+import Philosophy from "./pages/Philosophy";
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element>
+          <Route index element={<Navigate replace to="home" />} />
+          <Route path="home" element={<Home />} />
+          <Route path="shop" element={<Shop />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="blog" element={<Blog />} />
+          <Route path="philosophy" element={<Philosophy />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
