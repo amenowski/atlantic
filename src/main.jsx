1ea53054c145bda379fd924a095c994bfdc95c30
contactserus@gmail.com <[contact-serus@gmail.com]>
2024-01-12 13:19:28 +0100

    fix: setting up providers

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { CartPreviewProvider } from './contexts/CartPreviewProvider.jsx';
+import { MobileNavProvider } from './contexts/MobileNavProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CartPreviewProvider>
+      <MobileNavProvider>
+        <App />
+      </MobileNavProvider>
+    </CartPreviewProvider>
   </React.StrictMode>,
-)
+);
