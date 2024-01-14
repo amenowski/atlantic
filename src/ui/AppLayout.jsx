b664cc8592d3ab076a3a7b9af948741c1ea1b08d
amenowski <contactserus@gmail.com>
2024-01-14 20:05:57 +0100

    style: delete min-width in main container

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -5,7 +5,7 @@ function AppLayout() {
   return (
     <>
       <Header />
-      <main className="mx-auto min-w-[690px] max-w-[1400px] p-4">
+      <main className="mx-auto max-w-[1400px] p-4">
         <Outlet />
       </main>
     </>
