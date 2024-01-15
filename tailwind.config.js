331f9a6fd1428f612613cce6335900e45cc54628
amenowski <contactserus@gmail.com>
2024-01-15 20:14:00 +0100

    style: create new custom font madera

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        'madera-regular': ['"madera-regular", "sans-serif"'],
+        'madera-bold': ['"madera-bold", "sans-serif"'],
+      },
       colors: {
         primary: '#713434',
       },
