6ca018985b80c6a1c26d30687c1e9b489fb7570c
amenowski <contactserus@gmail.com>
2024-01-15 16:30:16 +0100

    style: button is more reusable, width is not contants

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom';
 
-function Button({ children, disabled, to, type, onClick }) {
-  const base = `uppercase py-3 px-4 border border-primary w-1/2 text-center tracking-widest text-sm transition-all duration-300`;
+function Button({ children, disabled, to, type, onClick, width }) {
+  const base = `uppercase py-4 px-4 border border-primary text-center tracking-widest text-sm transition-all duration-300`;
 
   const styles = {
-    primary: base + ' bg-primary text-white hover:bg-[#662e2e]',
-    secondary: base + ' bg-white text-primary hover:bg-[#eae1e1]',
+    primary:
+      base +
+      ` bg-primary text-white hover:bg-[#662e2e] text-sm tracking-wide sm:text-base sm:tracking-widest w-${width} `,
+    secondary: base + ` bg-white text-primary hover:bg-[#eae1e1] w-${width} `,
   };
 
   if (to)
