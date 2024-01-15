9502fb56636c3e4be5192e675746762c22ad4984
amenowski <contactserus@gmail.com>
2024-01-15 16:32:20 +0100

    feat: new resuable Spinner component to display when state is loading

---
+++ src/ui/Spinner.jsx
@@ -0,0 +1,11 @@
+import { PuffLoader } from 'react-spinners';
+
+function Spinner() {
+  return (
+    <div className="flex items-center justify-center">
+      <PuffLoader color="#000000" />
+    </div>
+  );
+}
+
+export default Spinner;
