438002db89cefe8d3c99de848ab4b3098f7b9eff
amenowski <contactserus@gmail.com>
2024-01-15 16:37:43 +0100

    feat: create QueryClient with ReactQuery to fetch products from data base

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,36 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Shop from "./pages/Shop";
-import Cart from "./pages/Cart";
-import Blog from "./pages/Blog";
-import Philosophy from "./pages/Philosophy";
-import AppLayout from "./ui/AppLayout";
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import Cart from './pages/Cart';
+import Blog from './pages/Blog';
+import Philosophy from './pages/Philosophy';
+import AppLayout from './ui/AppLayout';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route index element={<Navigate replace to="home" />} />
-          <Route path="home" element={<Home />} />
-          <Route path="shop" element={<Shop />} />
-          <Route path="cart" element={<Cart />} />
-          <Route path="blog" element={<Blog />} />
-          <Route path="philosophy" element={<Philosophy />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="home" />} />
+            <Route path="home" element={<Home />} />
+            <Route path="shop" element={<Shop />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="blog" element={<Blog />} />
+            <Route path="philosophy" element={<Philosophy />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
