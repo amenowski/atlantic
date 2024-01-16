0f6d9453ec49c60a2b1d317449949c2ee12346fa
amenowski <contactserus@gmail.com>
2024-01-16 21:23:57 +0100

    feat: create provider to react redux toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,16 @@ import App from './App.jsx';
 import './index.css';
 import { CartPreviewProvider } from './contexts/CartPreviewProvider.jsx';
 import { MobileNavProvider } from './contexts/MobileNavProvider.jsx';
+import { Provider } from 'react-redux';
+import { store } from './store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CartPreviewProvider>
       <MobileNavProvider>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </MobileNavProvider>
     </CartPreviewProvider>
   </React.StrictMode>,
