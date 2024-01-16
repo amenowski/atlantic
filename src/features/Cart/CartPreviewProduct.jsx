5109c757d1765ea7131a9bbd53be44003b05da89
amenowski <contactserus@gmail.com>
2024-01-16 21:25:31 +0100

    feat: cart preview product component

---
+++ src/features/Cart/CartPreviewProduct.jsx
@@ -0,0 +1,31 @@
+import { Link } from 'react-router-dom';
+import {
+  formatCurrency,
+  formatNameForURL,
+  revertFormattedName,
+} from '../../utils/helpers';
+import RemoveProduct from './RemoveProduct';
+
+function CartPreviewProduct({ product }) {
+  const { images, price, name, quantity, id } = product;
+
+  return (
+    <li className="flex gap-4 py-4 text-gray-500">
+      <Link to={`/product/${formatNameForURL(name)} `}>
+        <img src={images[0]} alt={name} className="w-56" />
+      </Link>
+      <div className="flex w-full flex-col">
+        <h5 className="mb-1 text-black">{revertFormattedName(name)}</h5>
+        <p className="mb-14">Qty: {quantity}</p>
+        <div className="flex items-center justify-between">
+          <p className="justify-end">
+            {formatCurrency(price)} x {quantity}
+          </p>
+          <RemoveProduct id={id} />
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export default CartPreviewProduct;
