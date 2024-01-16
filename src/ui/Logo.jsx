1e8c5b1efd876436e2467ae04b3bca4f25266974
amenowski <contactserus@gmail.com>
2024-01-16 21:14:56 +0100

    fix: logo is now link to home page

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom';
 import logo from '../assets/images/logo.png';
 
 function Logo() {
-  return <img src={logo} className={`h-20 lg:h-10`} alt="Store logo" />;
+  return (
+    <Link to="/home">
+      <img src={logo} className={`h-20 lg:h-10`} alt="Store logo" />
+    </Link>
+  );
 }
 
 export default Logo;
