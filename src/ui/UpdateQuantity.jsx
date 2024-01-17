5fd12a245f63d0ebea8eecac177dd05b4633776a
amenowski <contactserus@gmail.com>
2024-01-17 16:43:46 +0100

    feat: resuable component to change product quantity

---
+++ src/ui/UpdateQuantity.jsx
@@ -0,0 +1,25 @@
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  decQuantity,
+  getCurrentProductQuantity,
+  incQuantity,
+} from '../features/Cart/cartSlice';
+
+function UpdateQuantity({ id }) {
+  const dispatch = useDispatch();
+  const currentQuantity = useSelector(getCurrentProductQuantity(id));
+
+  return (
+    <div className="flex min-w-[90px] items-center justify-between gap-2 border p-2 ">
+      <button className="text-xl" onClick={() => dispatch(incQuantity(id))}>
+        +
+      </button>
+      <span className="flex-shrink-0">{currentQuantity}</span>
+      <button className="text-xl" onClick={() => dispatch(decQuantity(id))}>
+        -
+      </button>
+    </div>
+  );
+}
+
+export default UpdateQuantity;
