71231f44188ef28a46ccbf54854edbe3af3c721c
amenowski <contactserus@gmail.com>
2024-01-17 16:45:27 +0100

    style: display table component

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,11 @@
+import CartTable from '../features/Cart/CartTable';
+
 function Cart() {
-  return <div></div>;
+  return (
+    <div className="min-h-96">
+      <CartTable />
+    </div>
+  );
 }
 
 export default Cart;
