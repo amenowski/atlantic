c5e8dc0fd61d1d6ba3de0233202a531920016ec4
amenowski <contactserus@gmail.com>
2024-01-17 16:42:20 +0100

    style: add min height to container

--- src/ui/NewArrivals.jsx
+++ src/ui/NewArrivals.jsx
@@ -5,13 +5,14 @@ import Spinner from './Spinner';
 function NewArrivals() {
   const { products, isLoading } = useProducts();
 
-  if (isLoading) return <Spinner />;
-
   return (
-    <div className="py-24">
-      <h3 className="text-center text-lg">NEW ARRIVALS FOR MODERN LIVING</h3>
-      <div className="grid grid-cols-1 gap-8 py-12 sm:grid-cols-2 lg:grid-cols-4">
-        {products.map((product) => (
+    <div className="min-h-[400px] py-24">
+      <h3 className="pb-8 text-center text-lg">
+        NEW ARRIVALS FOR MODERN LIVING
+      </h3>
+      {isLoading && <Spinner />}
+      <div className="grid grid-cols-1 gap-8 py-4 sm:grid-cols-2 lg:grid-cols-4">
+        {products?.map((product) => (
           <Product product={product} key={product.id} />
         ))}
       </div>
