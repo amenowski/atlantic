391cdc30bc02d561589bc49df86ddcef0866bfdb
amenowski <contactserus@gmail.com>
2024-01-18 20:35:45 +0100

    style: add ShopOperations and ShopProducts to shop page view

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,5 +1,13 @@
+import ShopOperations from '../features/Shop/ShopOperations';
+import ShopProducts from '../features/Shop/ShopProducts';
+
 function Shop() {
-  return <div></div>;
+  return (
+    <div className="grid min-h-[450px] grid-cols-1 gap-8 py-12 md:grid-cols-[0.2fr_1fr]">
+      <ShopOperations />
+      <ShopProducts />
+    </div>
+  );
 }
 
 export default Shop;
