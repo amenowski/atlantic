65b618ae0be235d87a105b4c81be357ea3f8cafd
amenowski <contactserus@gmail.com>
2024-01-18 19:55:15 +0100

    style: change cart item to be resposive on mobile etc..

--- src/features/Cart/CartTableItem.jsx
+++ src/features/Cart/CartTableItem.jsx
@@ -11,8 +11,8 @@ function CartTableItem({ product }) {
   const { name, price, totalPrice, id, images, size } = product;
 
   return (
-    <div className="grid grid-cols-[1fr_0.7fr_0.7fr_0.3fr] place-items-center py-8 text-lg ">
-      <div className="flex h-full w-full items-center gap-8">
+    <div className="grid  grid-cols-[1fr_0.7fr_0.7fr] grid-rows-2 items-center py-8 text-lg md:grid-cols-[1fr_0.7fr_0.7fr_0.3fr] md:grid-rows-1 md:place-items-center ">
+      <div className="col-span-4 flex h-full w-full items-center gap-8 md:col-span-1">
         <Link to={`/product/${name}`}>
           <img
             width={140}
@@ -31,12 +31,19 @@ function CartTableItem({ product }) {
           </Link>
         </span>
       </div>
-      <span>{formatCurrency(price)}</span>
-      <div className="flex flex-col gap-1">
+      <div className="flex flex-col gap-1 text-left">
+        <p className="text-sm normal-case text-gray-500 md:hidden">Price</p>
+        <span>{formatCurrency(price)}</span>
+      </div>
+      <div className="gap- flex flex-col gap-1">
         <UpdateQuantity id={id} />
         <RemoveProduct id={id} />
       </div>
-      <span>{formatCurrency(totalPrice)}</span>
+      <div className="flex flex-col gap-1 text-right">
+        <p className="text-sm normal-case text-gray-500 md:hidden">Total</p>
+
+        <span>{formatCurrency(totalPrice)}</span>
+      </div>
     </div>
   );
 }
