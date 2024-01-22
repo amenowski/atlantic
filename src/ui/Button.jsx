95ddec1cc17f89ae16af356b0581a3448affd7dd
amenowski <contactserus@gmail.com>
2024-01-22 19:12:03 +0100

    style: add new type, button small with smaller padding

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,13 +1,16 @@
 import { Link } from 'react-router-dom';
 
 function Button({ children, disabled, to, type, onClick, width }) {
-  const base = `uppercase py-4 px-4 border border-primary text-center tracking-widest text-sm transition-all duration-300`;
+  const base = `flex items-center gap-2 uppercase py-4 px-4 border border-primary text-center tracking-widest text-sm transition-all duration-300`;
 
   const styles = {
     primary:
       base +
       ` bg-primary text-white hover:bg-[#662e2e] text-sm tracking-wide sm:text-base sm:tracking-widest w-${width} `,
     secondary: base + ` bg-white text-primary hover:bg-[#eae1e1] w-${width} `,
+    primarySmall:
+      base +
+      ` p-2 bg-primary text-white hover:bg-[#662e2e] text-sm tracking-wide sm:text-base sm:tracking-widest w-${width} `,
   };
 
   if (to)
