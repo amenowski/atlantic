1666f3deb49e2b61896f01fff1231adb1c618ef4
amenowski <contactserus@gmail.com>
2024-01-23 17:34:53 +0100

    feat: fetch newest products with new function

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,11 +1,57 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { getProducts } from '../services/apiProducts';
+import { useSearchParams } from 'react-router-dom';
+import { PAGE_SIZE } from '../utils/constants';
 
 export function useProducts() {
-  const { data: products, isLoading } = useQuery({
-    queryKey: ['products'],
-    queryFn: getProducts,
+  const queryClient = useQueryClient();
+  const [searchParams] = useSearchParams();
+
+  // FILTER
+  const filterValue = searchParams.get('isDiscount');
+  const filter =
+    !filterValue || filterValue === 'all'
+      ? null
+      : { field: 'isDiscount', value: filterValue };
+
+  // SORTBY
+  const sortByRow = searchParams.get('sortBy') || 'name-asc';
+  const [field, direction] = sortByRow.split('-');
+  const sortBy = { field, direction };
+
+  // PAGINATION
+  const page = !searchParams.get('page') ? 1 : Number(searchParams.get('page'));
+
+  // CATEGORIES
+  const categoriesParam = searchParams.get('categories');
+  const categoriesValue = categoriesParam ? categoriesParam.split('-') : [];
+  const categories = categoriesValue.length > 0 ? categoriesValue : null;
+  console.log(categories);
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['products', filter, sortBy, page, categories],
+    queryFn: () => getProducts({ filter, sortBy, page, categories }),
   });
 
-  return { products, isLoading };
+  const products = data?.products;
+  const count = data?.count;
+
+  // PRE-FETCHING
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+
+  if (page < pageCount)
+    queryClient.prefetchQuery({
+      queryKey: ['products', filter, sortBy, page + 1, categories],
+      queryFn: () =>
+        getProducts({ filter, sortBy, page: page + 1, categories }),
+    });
+
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryKey: ['products', filter, sortBy, page + 1, categories],
+      queryFn: () =>
+        getProducts({ filter, sortBy, page: page + 1, categories }),
+    });
+
+  return { products, isLoading, count };
 }
--- src/ui/NewArrivals.jsx
+++ src/ui/NewArrivals.jsx
@@ -1,9 +1,11 @@
-import { useProducts } from '../hooks/useProducts';
+import { useNewestProducts } from '../hooks/useNewestProducts';
 import Product from './Product';
 import Spinner from './Spinner';
 
 function NewArrivals() {
-  const { products, isLoading } = useProducts();
+  const { newestProducts, isLoading } = useNewestProducts(4);
+
+  console.log(newestProducts);
 
   return (
     <div className="min-h-[400px] py-24">
@@ -12,7 +14,7 @@ function NewArrivals() {
       </h3>
       {isLoading && <Spinner />}
       <div className="grid grid-cols-1 gap-8 py-4 sm:grid-cols-2 lg:grid-cols-4">
-        {products?.map((product) => (
+        {newestProducts?.map((product) => (
           <Product product={product} key={product.id} />
         ))}
       </div>
