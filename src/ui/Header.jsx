e1bd201943bb032efac7b5ede8e0e79281094b8a
amenowski <contactserus@gmail.com>
2024-01-28 21:19:56 +0100

    feat: add login button when user is not authenticated and logout when he is it

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,17 +1,23 @@
 import Logo from './Logo';
-import Navigation from './Navigation';
-import { IoBagOutline } from 'react-icons/io5';
-import { AiOutlineMenu } from 'react-icons/ai';
 import { useCartPreview } from '../contexts/CartPreviewProvider';
-import CartPreview from '../features/Cart/CartPreview';
-import Overlay from './Overlay';
 import { AnimatePresence } from 'framer-motion';
 import { useMobileNav } from '../contexts/MobileNavProvider';
+import { Link } from 'react-router-dom';
+import { useUser } from '../hooks/useUser';
+
+import Navigation from './Navigation';
+import CartPreview from '../features/Cart/CartPreview';
+import Overlay from './Overlay';
 import MobileNav from './MobileNav';
+import Logout from './Logout';
+
+import { IoBagOutline } from 'react-icons/io5';
+import { AiOutlineMenu, AiOutlineUser } from 'react-icons/ai';
 
 function Header() {
   const { onSetIsOpenCart, isCartOpen } = useCartPreview();
   const { onSetIsOpenNav, isNavOpen } = useMobileNav();
+  const { isAuthenticated } = useUser();
 
   function handleOpenCart() {
     onSetIsOpenCart(true);
@@ -24,18 +30,29 @@ function Header() {
   return (
     <header className="flex w-full items-center justify-between px-6 py-5">
       <AiOutlineMenu
-        size={25}
-        className="block cursor-pointer lg:hidden"
+        size={35}
+        className="block cursor-pointer  rounded-md p-1  transition-all duration-300 hover:bg-gray-100 lg:hidden"
         onClick={handleOpenNav}
       />
       <div className="flex items-center gap-3">
         <Logo />
         <Navigation />
       </div>
-      <div>
+      <div className="flex items-center gap-2">
+        {isAuthenticated ? (
+          <Logout />
+        ) : (
+          <Link to="/login">
+            <AiOutlineUser
+              className="cursor-pointer rounded-md  p-1 transition-all duration-300 hover:bg-gray-100"
+              size={35}
+            />
+          </Link>
+        )}
+
         <IoBagOutline
-          size={25}
-          className="cursor-pointer"
+          className="cursor-pointer rounded-md  p-1 hover:bg-gray-100"
+          size={35}
           onClick={handleOpenCart}
         />
         <AnimatePresence>
