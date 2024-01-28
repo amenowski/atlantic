445ee961d58c503fa1d067104bb9a3958cfa0fc1
amenowski <contactserus@gmail.com>
2024-01-28 21:04:48 +0100

    style: change media query of grid columns on large

--- src/features/Shop/ShopProducts.jsx
+++ src/features/Shop/ShopProducts.jsx
@@ -10,7 +10,7 @@ function ShopProducts() {
 
   return (
     <div className="flex flex-col gap-2">
-      <div className="grid gap-8 sm:grid-cols-2 md:grid-cols-3">
+      <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
         {products?.map((product) => (
           <Product product={product} key={product.id} />
         ))}
