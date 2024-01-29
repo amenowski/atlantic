add80213f46b428f6bb981e4bf3563c823af1619
amenowski <contactserus@gmail.com>
2024-01-29 20:06:19 +0100

    style: add logo component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,10 @@
 import LoginForm from '../features/auth/LoginForm';
+import Logo from '../ui/Logo';
 
 function Login() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center  gap-4 bg-gray-50">
+      <Logo />
       <h1 className="text-2xl font-bold">Login to atlantic</h1>
       <LoginForm />
     </div>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,10 @@
 import SignupForm from '../features/auth/SignupForm';
+import Logo from '../ui/Logo';
 
 function Signup() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center  gap-4 bg-gray-50">
+      <Logo />
       <h1 className="text-2xl font-bold">Sign up to atlantic</h1>
       <SignupForm />
     </div>
